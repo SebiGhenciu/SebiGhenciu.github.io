ebd3e55b03e0afa1b8e253e71fc7bf6050bdcd57
SKanae-afk <sebyghenciu5@gmail.com>
2022-07-04 00:34:17 +0300

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [],
+  content: [
+    './index.html',
+    './gallery/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+}
